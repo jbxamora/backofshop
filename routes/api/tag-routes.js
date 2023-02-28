b9dd848ccb5a02d8ff649df701e73add17771f0c
jbxamora <jbxamora@icloud.com>
2023-02-28 02:14:23 -0800

    created tag put + delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,12 +57,47 @@ router.post('/', (req, res) => {
 
 // Update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
