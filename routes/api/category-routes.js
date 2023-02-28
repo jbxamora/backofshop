5d775c126cebe491ed58a7d7591db78d13d8415a
jbxamora <jbxamora@icloud.com>
2023-02-28 00:53:53 -0800

    updated category model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,10 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // find all categories be sure to include its associated Products
+  
 });
 
 router.get('/:id', (req, res) => {
